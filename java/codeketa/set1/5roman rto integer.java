12c6e20d50d518fb3dd881a9a56d9cdd97270747
ragulasakthivel <34960334+ragulasakthivel@users.noreply.github.com>
2018-02-22 21:39:40 +0530

    Create 5roman rto integer.java

---
+++ java/codeketa/set1/5roman rto integer.java
@@ -0,0 +1,55 @@
+//'main' method must be in a class 'Rextester'.
+//Compiler version 1.8.0_111
+
+import java.util.*;
+import java.lang.*;
+
+class Rextester
+{  
+    public static void main(String args[])
+    {
+       Scanner sc=new Scanner(System.in);
+       String s=sc.nextLine();
+       char[] a=new char[100];
+      
+       int i,b=0,sum=0;
+       a=s.toCharArray();
+        
+        for(i=0;i<s.length();i++)
+        { 
+         
+         
+        if(a[i]=='I' && a[i+1]=='V')
+        {
+            b=4;
+            break;
+            
+        }  
+        
+            if((a[i]=='I') && (a[i+1]=='X'))
+        {
+            b=9;
+            
+            break;
+        } 
+        else
+        {
+        switch(a[i])
+            {
+            case 'I':
+                  b=1;
+                  break;
+            case 'V':
+                  b=5;
+                  break;
+            case 'X':
+                  b=10;
+                  break;
+             }
+        }
+           sum=sum+b;  
+           
+           }
+         System.out.println(sum);
+        }
+        }
