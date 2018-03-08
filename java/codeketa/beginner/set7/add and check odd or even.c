a695f05da671f3e3ed62e2163575ed59d2aa38d8
ragulasakthivel <34960334+ragulasakthivel@users.noreply.github.com>
2018-03-08 23:33:47 +0530

    Create add and check odd or even.c

---
+++ java/codeketa/beginner/set7/add and check odd or even.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+    int m,n,sum=0;
+    scanf("%d %d",&m,&n);
+    sum=m+n;
+    if(sum%2==0)
+      printf("even");
+     else
+      printf("odd");
+    return 0;
+}
