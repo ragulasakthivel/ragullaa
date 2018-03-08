32d910c39638a49436175790d71da01371c4d9e2
ragulasakthivel <34960334+ragulasakthivel@users.noreply.github.com>
2018-03-08 22:20:47 +0530

    Create print th str k times.c

---
+++ java/codeketa/beginner/set4/print th str k times.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{ 
+int n,i;
+scanf("%d",&n);
+char str[100];
+scanf("%s",str);
+for(i=0;i<n;i++)
+    {
+        printf("%s\n",str);
+    }
+return 0;
+	
+}
+
